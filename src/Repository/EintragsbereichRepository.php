6fa18107e7df61791193cfb5c572d2f1acbc2a24
unknown <c.borchard@berlin.de>
2019-06-07 12:13:09 +0200

    EintragsbereichEntity

---
+++ src/Repository/EintragsbereichRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Eintragsbereich;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Eintragsbereich|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Eintragsbereich|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Eintragsbereich[]    findAll()
+ * @method Eintragsbereich[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EintragsbereichRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Eintragsbereich::class);
+    }
+
+    // /**
+    //  * @return Eintragsbereich[] Returns an array of Eintragsbereich objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('e.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Eintragsbereich
+    {
+        return $this->createQueryBuilder('e')
+            ->andWhere('e.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
